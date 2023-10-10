4a262eb2b2db2661cf184a5f956a7cb6f899f1c9
Lakrandi-Ayeshani <lakrandiayeshani1204@gmail.com>
2023-10-11 02:24:44 +0530

    deletet task function done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
-import { task } from './data/data';
+import { tasks as initialTasks } from './data/data';
 import TaskList from './screens/TaskList';
+import { useState } from 'react';
 
 function App() {
-  const tasks = task;
+  const [tasks, setTasks] = useState(initialTasks);
+  const deleteTask = (id) => {
+    const updateTasks = tasks.filter((oneTask) => oneTask._id !== id);
+    setTasks(updateTasks);
+  }
 
   return (
-    <div className="App container p-5">
-      <TaskList tasks={tasks}/>
+    <div className="App p-5">
+      <TaskList tasks={tasks} deleteTask={deleteTask}/>
     </div>
   );
 }
