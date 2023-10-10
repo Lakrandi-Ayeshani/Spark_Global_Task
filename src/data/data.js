d6fb2d071246651acabfd1b62dcaa1632661d39d
Lakrandi-Ayeshani <lakrandiayeshani1204@gmail.com>
2023-10-10 14:23:02 +0530

    data set was created

---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+export const task = [
+    {
+        "_id": 1,
+        "title": "title 1",
+        "description":  "description 1"
+    },
+    {
+        "_id": 1,
+        "title": "title 1",
+        "description":  "description 1"
+    },
+    {
+        "_id": 1,
+        "title": "title 1",
+        "description":  "description 1"
+    },
+    {
+        "_id": 1,
+        "title": "title 1",
+        "description":  "description 1"
+    },
+    {
+        "_id": 1,
+        "title": "title 1",
+        "description":  "description 1"
+    },
+    {
+        "_id": 1,
+        "title": "title 1",
+        "description":  "description 1"
+    },
+    {
+        "_id": 1,
+        "title": "title 1",
+        "description":  "description 1"
+    },
+    {
+        "_id": 1,
+        "title": "title 1",
+        "description":  "description 1"
+    },
+    {
+        "_id": 1,
+        "title": "title 1",
+        "description":  "description 1"
+    },{
+        "_id": 1,
+        "title": "title 1",
+        "description":  "description 1"
+    }
+];
