030ebd3a752f07c6cc5b96f3c0513c52b628560c
Lakrandi-Ayeshani <lakrandiayeshani1204@gmail.com>
2023-10-10 14:23:45 +0530

    data fetched and passed to component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { task } from './data/data';
+import TaskList from './screens/TaskList';
 
 function App() {
+  const tasks = task;
+
   return (
-    <div className="App">
-      Task App
+    <div className="App container p-5">
+      <TaskList tasks={tasks}/>
     </div>
   );
 }
